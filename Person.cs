18f11b1fb1fd8dc0d6ce5a6007931ac63d629e3a
lbabiuch <luki.babiuch@gmail.com>
2020-01-25 14:47:28 +0100

    first

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+
+        public Person(
+            int id,
+            string name,
+            string lastName,
+            string phone)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Phone = phone;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("data.txt");
+            List<Person> people = lines.Select(x =>
+            {
+                var data = x.Split(';');
+                return new Person(Convert.ToInt32(data[0]), data[1], data[2], data[3]);
+            }).ToList();
+
+            var SortedPeople = people.OrderBy(x => x.LastName).ThenBy(x => x.Name);
+
+            using (StreamWriter writer = new StreamWriter("result.txt"))
+            {
+                foreach (var item in SortedPeople)
+                {
+                    writer.WriteLine($"[{item.Id}] {item.Name} {item.LastName} : {item.Phone}");
+                }
+            }
+            /*using StreamWriter writer = new StreamWriter("result.txt");
+            foreach (var item in SortedPeople)
+            {
+                writer.WriteLine($"[{item.Id}] {item.Name} {item.LastName} : {item.Phone}");
+            }*/
+
+            //writer.Close();
+        }
+    }
+}
